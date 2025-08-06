32fff6212074a607cc1aa63e4bdfc964be18a92b
Rachit Agrahari <138610648+RachitAgr13@users.noreply.github.com>
2025-08-06 22:31:58 +0530

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+ // treat all JS code as newer version
+
+ let name = "Rachit" //string
+ let age = 18 // integer
+ let isLoggedIn = false // boolean
+
+ // null => standalone value (empty value)
+ // undefined => 
+// symbol => uniqueness
+
+ // object 
+
+ console.log(typeof null)
