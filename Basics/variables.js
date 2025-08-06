4fefc8813d620fcac8b404f1f3f2fc3cb4dfe1db
Rachit Agrahari <138610648+RachitAgr13@users.noreply.github.com>
2025-08-06 22:04:52 +0530

    Node modules

---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 6969
+let accName = "Rachit"
+var accountCity = "Kanpur"
+
+// do not prefer "var"
+
+let accountState; //value will be undefined if not assigned at the time of declarartion.
+
+console.log(accName)
